42f9c706bcf9bed6937d233fb548b396efca5747
vrk0ranit <ranitbag49@gmail.com>
2024-04-04 20:30:18 +0530

    add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,24 +1,29 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  signInStart,
+  signinSuccess,
+  signInFailure,
+} from '../redux/user/userSlice';
 
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if ( !formData.email || !formData.password) {
-      return setErrorMessage('Please fill out all fields.');
+    if (!formData.email || !formData.password) {
+      return dispatch(signInFailure('Please fill all the fields'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -26,16 +31,16 @@ export default function SignIn() {
       });
       const data = await res.json();
       if (data.success === false) {
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
-      setLoading(false);
-      if(res.ok) {
+
+      if (res.ok) {
+        dispatch(signinSuccess(data));
         navigate('/');
       }
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
-    }
+      dispatch(signInFailure(error.message));
+    } 
   };
   return (
     <div className='min-h-screen mt-20'>
@@ -44,7 +49,7 @@ export default function SignIn() {
         <div className='flex-1'>
           <Link to='/' className='font-bold dark:text-white text-4xl'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-              Ranit's
+              Sahand's
             </span>
             Blog
           </Link>
@@ -57,7 +62,6 @@ export default function SignIn() {
 
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
-            
             <div>
               <Label value='Your email' />
               <TextInput
@@ -71,7 +75,7 @@ export default function SignIn() {
               <Label value='Your password' />
               <TextInput
                 type='password'
-                placeholder='************'
+                placeholder='**********'
                 id='password'
                 onChange={handleChange}
               />
@@ -93,9 +97,9 @@ export default function SignIn() {
             
           </form>
           <div className='flex gap-2 text-sm mt-5'>
-            <span>Don't Have an account?</span>
+            <span>Dont Have an account?</span>
             <Link to='/sign-up' className='text-blue-500'>
-              Sign up
+              Sign Up
             </Link>
           </div>
           {errorMessage && (
---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false
+  }
+
+  export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+     
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+            state.error = null;
+          },
+          signinSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+          },
+          signInFailure: (state, action) => {
+            state.loading = false;
+            state.error = action.payload;
+          },
+    }
+  })
+
+  export const { signInStart, signinSuccess, signInFailure} = userSlice.actions;
+
+  export default userSlice.reducer

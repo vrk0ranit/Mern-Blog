2409c8510601c90cfa3ad2eb309551aa843189fa
vrk0ranit <ranitbag49@gmail.com>
2024-04-06 14:48:52 +0530

    complete sidebar of the dashboard

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -32,7 +32,7 @@ export default function Header() {
       </Button>
       <div className='flex gap-2 md:order-2'>
         <Button className='w-12 h-10 hidden sm:inline' color="gray" pill onClick={() => dispatch(toggleTheme())}>
-        {theme === 'light' ? <FaSun /> : <FaMoon />}
+        {theme === 'light' ? <FaMoon /> : <FaSun />}
         </Button>
         {currentUser ? (
           <Dropdown

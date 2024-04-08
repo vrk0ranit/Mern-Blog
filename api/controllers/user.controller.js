99b0ad9faa52676151d9e627763a7716f73b2f45
vrk0ranit <ranitbag49@gmail.com>
2024-04-08 08:27:08 +0530

    add delete user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -55,3 +55,15 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
+export const deleteUser = async (req, res, next) => {
+  if(req.user.id != req.params.userId){
+    return errorHandler(403, 'You are not allow to delete the account')
+  }
+  try {
+    await User.findByIdAndDelete(req.params.userId)
+    res.status(200).json('User has been deleted')
+  } catch (error) {
+    next(error)
+  }
+}
+

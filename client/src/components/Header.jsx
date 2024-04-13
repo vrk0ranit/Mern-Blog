85ff85792366c6eba82f67bc73bc826abaa72d97
vrk0ranit <ranitbag49@gmail.com>
2024-04-13 16:55:01 +0530

    header darkmode update for responsiveness

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -64,11 +64,9 @@ export default function Header() {
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </form>
-      <Button className='w-12 h-10 lg:hidden' color="gray" pill>
-        <AiOutlineSearch />
-      </Button>
+      
       <div className='flex gap-2 md:order-2'>
-        <Button className='w-12 h-10 hidden sm:inline' color="gray" pill onClick={() => dispatch(toggleTheme())}>
+        <Button className='w-12 h-10  sm:inline' color="gray" pill onClick={() => dispatch(toggleTheme())}>
         {theme === 'light' ? <FaMoon /> : <FaSun />}
         </Button>
         {currentUser ? (
